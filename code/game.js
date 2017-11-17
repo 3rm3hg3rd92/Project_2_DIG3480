adb90dd99ca8aceb3794f4cfb7e928e05a9a30f7
3rm3hg3rd92 <33039070+3rm3hg3rd92@users.noreply.github.com>
2017-11-16 19:13:13 -0500

    Project_2 enemy.fix

--- code/game.js
+++ code/game.js
@@ -362,16 +362,6 @@ var maxStep = 0.05;
 
 var playerXSpeed = 7;
 
-Platform.prototype.act = function(step, level) {
-	
-	var newPos = this.pos.plus(this.speed.times(step));
-  if (!level.obstacleAt(newPos, this.size))
-    this.pos = newPos;
-  else if (this.repeatPos)
-    this.pos = this.repeatPos;
-  else
-    this.speed = this.speed.times(-1);
-}
 
 Player.prototype.moveX = function(step, level, keys) {
   this.speed.x = 0;
